024833019c5785da676ebd607fb797e37d65f91b
ChiHuy177 <huynguyenbd54@gmail.com>
2025-10-14 15:21:41 +0700

    rename folder Q4

--- Lab 1/Q4/App.tsx
+++ Lab 1/Q4/App.tsx
@@ -1,12 +1,12 @@
 import { ScrollView, Text } from 'react-native';
 import { styles } from './style';
-import data from './Data';
+import data from './data/Data';
 import Square from './components/Square';
 
 function App() {
   return (
     <ScrollView style={styles.container}>
-      <Text>Nguyễn Chí Huy</Text>
+      <Text>Nguyễn Chí Huy Q4</Text>
       {data.map((item, index) => (
         <Square key={index} text={`Square ${item}`} />
       ))}
