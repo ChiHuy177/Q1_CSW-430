eaa4472e776bf570b98ecb567bb50f01eeb2337c
ChiHuy177 <huynguyenbd54@gmail.com>
2025-10-14 15:12:47 +0700

    Add q4

---
+++ Lab 1/Lab1/App.tsx
@@ -0,0 +1,17 @@
+import { ScrollView, Text } from 'react-native';
+import { styles } from './style';
+import data from './Data';
+import Square from './components/Square';
+
+function App() {
+  return (
+    <ScrollView style={styles.container}>
+      <Text>Nguyễn Chí Huy</Text>
+      {data.map((item, index) => (
+        <Square key={index} text={`Square ${item}`} />
+      ))}
+    </ScrollView>
+  );
+}
+
+export default App;
