5415920a20514633453856de5438db301bf61b15
ChiHuy177 <huynguyenbd54@gmail.com>
2025-10-20 10:13:11 +0700

    Q6

--- Lab 1/Q4/App.tsx
+++ Lab 1/Q4/App.tsx
@@ -4,6 +4,8 @@ import data from './data/Data';
 import Square from './components/Square';
 import SumLastFirstDigit from './Q6/SumFirstLastDigit';
 import MinOfThreeNumbers from './Q6/MinimumOfThreeNum';
+import EmployeeInfo from './Q6/EmployeeInfo';
+import HailstoneSequence from './Q6/HailstoneSequence';
 
 function App() {
   return (
@@ -12,8 +14,11 @@ function App() {
       {data.map((item, index) => (
         <Square key={index} text={`Square ${item}`} />
       ))} */}
-      <SumLastFirstDigit/>
-      <MinOfThreeNumbers/>
+      {/* <SumLastFirstDigit /> */}
+      {/* <MinOfThreeNumbers /> */}
+      {/* <Text style={styles.title}>Q6 - Employee Info</Text>
+      <EmployeeInfo title="Employee Information Form" /> */}
+      <HailstoneSequence />
     </ScrollView>
   );
 }
